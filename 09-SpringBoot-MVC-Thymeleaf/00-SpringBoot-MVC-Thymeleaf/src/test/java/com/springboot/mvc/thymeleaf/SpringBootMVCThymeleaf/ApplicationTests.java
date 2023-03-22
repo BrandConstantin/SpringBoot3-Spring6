044c7401ee2b98c935bd19b2f9602210c0f684ab
BrandConstantin <constantin.brindu@gmail.com>
2023-03-22 22:18:29 +0100

    Spring Boot - MVC - Thymeleaf

---
+++ 09-SpringBoot-MVC-Thymeleaf/00-SpringBoot-MVC-Thymeleaf/src/test/java/com/springboot/mvc/thymeleaf/SpringBootMVCThymeleaf/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mvc.thymeleaf.SpringBootMVCThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
