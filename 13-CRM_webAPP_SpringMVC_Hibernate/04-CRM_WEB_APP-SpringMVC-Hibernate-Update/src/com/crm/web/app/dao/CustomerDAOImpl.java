dd227c9af2cfaf368cda5addb319190451bbc416
BrandConstantin <constantin.brindu@gmail.com>
2023-03-31 11:45:29 +0200

    CRM WEB APP - SpringMVC & Hibernate - UPDATE

---
+++ 13-CRM_webAPP_SpringMVC_Hibernate/04-CRM_WEB_APP-SpringMVC-Hibernate-Update/src/com/crm/web/app/dao/CustomerDAOImpl.java
@@ -0,0 +1,60 @@
+package com.crm.web.app.dao;
+
+import java.util.List;
+
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crm.web.app.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+			
+	@Override
+	public List<Customer> getCustomers() {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+				
+		// create a query  ... sort by last name
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName", Customer.class);
+		
+		// execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+				
+		// return the results		
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		// get hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save the customer
+		//currentSession.save(theCustomer); 		// save
+		//currentSession.update(theCustomer);		// update
+		currentSession.saveOrUpdate(theCustomer); 	// save or update
+	}
+
+	@Override
+	public Customer getCustomers(int theId) {
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// read from database using PK
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
+	}
+	
+}
